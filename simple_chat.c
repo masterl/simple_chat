4290edbf8177404d2eaf3f65297036bf7431675a
Leonardo de Oliveira Lourenço <bigodation@gmail.com>
2015-05-11 01:03:13 -0300

    Added port validation function

--- simple_chat.c
+++ simple_chat.c
@@ -2,9 +2,43 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 void error(char *msg)
 {
     perror(msg);
     exit(1);
+}
+
+// 1-65535  all ports
+// 1-1023   reserved (root)
+int validade_and_convert_port(char *port)
+{
+    int port_number = 0;
+
+    if(strlen(port) > 5)
+    {
+        return SC_INVALID_PORT;
+    }
+
+    while(*port)
+    {
+        port_number *= 10;
+
+        if(!isdigit(*port))
+        {
+            return SC_INVALID_PORT;
+        }
+
+        port_number += (*port) - '0';
+        ++port;
+    }
+
+    if(port_number > 65535)
+    {
+        return SC_RANGE_ERROR;
+    }
+
+    return port_number;
 }
--- simple_chat.h
+++ simple_chat.h
@@ -1,6 +1,14 @@
 #ifndef SIMPLE_CHAT_H
 #define SIMPLE_CHAT_H
 
+enum
+{
+    SC_INVALID_PORT = -1,
+    SC_RANGE_ERROR = -2
+};
+
 void error(char *msg);
+
+int validade_and_convert_port(char *port);
 
 #endif
